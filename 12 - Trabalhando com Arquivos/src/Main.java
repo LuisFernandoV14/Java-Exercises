8dd07481429b383b1813f43f0464263b46e640f5
LuisFenandoV14 <venturaluisf7@outlook.com>
2025-10-02 11:32:04 -0300

    Adicionando um arquivo teste para ler arquivo .txt
    Esse arquivo contém apenas um modelo para abrir um Scanner, ler um
    arquivo .txt e imprimir as suas linhas no console.

---
+++ 12 - Trabalhando com Arquivos/src/Main.java
@@ -0,0 +1,41 @@
+// Esse é um teste para ler um arquivo
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Abre um objeto do tipo File e passa a receita de bolo pra ele, tem que usar "\\" para o compilador ler "\"
+        File arquivo = new File("C:\\Users\\SAMSUNG\\Documents\\Cursos\\Curso de Java\\Java-Exercises\\12 - Trabalhando com Arquivos\\Receita de bolo fofinho.txt");
+
+        // Cria um Scanner vazio
+        Scanner leitor = null;
+
+        // Atribuir um arquivo pra um leitor pode jogar uma IOException, então é recomendado sempre fazer isso num bloco try-catch
+        try {
+
+            leitor = new Scanner(arquivo);
+            while (leitor.hasNextLine()) {
+                System.out.println(leitor.nextLine());
+            }
+
+        }
+        catch (IOException e) {
+
+            System.out.println("Erro: " + e.getMessage());
+
+        }
+        finally {
+            if (leitor != null) {
+                leitor.close();
+            }
+        }
+
+        // Por fim, tem que fechar leitor, se ele for nulo não tem o que fechar, vai ser jogado uma exceção, por isso o if
+
+    }
+}
+
+        // Obs, caso tenha baixado ou copiado esse arquivo do meu repositório para a sua máquina corrija o caminho para abrir o objeto do tipo File, na linha 11, corretamente, com certeza o caminho não será o mesmo
